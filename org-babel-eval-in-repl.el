9db3d612dd92edd5ca5b6874cb2bb9b05671e864
Takeshi Teshima <takeshi.diadochos@gmail.com>
2016-11-19 15:21:53 +0900

    Update version

--- org-babel-eval-in-repl.el
+++ org-babel-eval-in-repl.el
@@ -1,7 +1,7 @@
 ;;; org-babel-eval-in-repl.el --- Eval org-mode babel code blocks in various REPLs.   -*- lexical-binding: t; -*-
 ;;
 ;; Author: Takeshi Teshima <diadochos.developer@gmail.com>
-;; Version:           20161119.1511
+;; Version:           20161119.1521
 ;; Package-Requires: ((eval-in-repl "20160418.1843") (org-plus-contrib "20161118") (emacs "24"))
 ;; Keywords: literate programming, reproducible research, async execution
 ;; URL: https://github.com/diadochos/org-babel-eval-in-repl
